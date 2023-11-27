622171cd9148211fd59e6a7b07cebcfe8edf2149
pvtamh2bg <pvtamitns.bg@gmail.com>
2023-11-27 23:51:56 +0700

    add test stage

--- cdk-cicd/lib/cdk-cicd-stack.ts
+++ cdk-cicd/lib/cdk-cicd-stack.ts
@@ -1,15 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'AwesomePipeline', {
+    const pipeline = new CodePipeline(this, 'AwesomePipeline', {
       pipelineName: 'AwesomePipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('pvtamh2bg/cdk', 'cicd-practice'),
+        input: CodePipelineSource.gitHub('pvtamh2bg/cdk', 'cicd-practive'),
         commands: [
           'cd cdk-cicd',
           'npm ci',
@@ -18,6 +19,9 @@ export class CdkCicdStack extends cdk.Stack {
         primaryOutputDirectory: 'cdk-cicd/cdk.out'
       })
     })
+    const testStage = pipeline.addStage(new PipelineStage(this, 'PipelineTestStage', {
+      stageName: 'test'
+    }))
 
   }
 }
